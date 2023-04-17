c24d8c53c714ac93fd5284199a8eb84a578d975c
aimisaka <aimixsaka@gmail.com>
2023-04-17 20:46:53 +0800

    feat: add api register full chain(model,router,service)

--- router/router.go
+++ router/router.go
@@ -3,13 +3,27 @@ package router
 import (
 	"net/http"
 
+	v1 "github.com/NJUPT-SAST/sast-link-backend/api/v1"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter() *gin.Engine {
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
+
+	apiV1 := r.Group("/api/v1")
+
+	usergroup := apiV1.Group("/user")
+	{
+		usergroup.POST("/register", v1.Register)
+	}
+
+	// admingroup := apiV1.Group("/admin")
+	// {
+	// }
+
 	return r
 }
