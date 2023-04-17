65b7ffed75bf7f99434aaf68998c77061bee6830
xun <xun794@gmail.com>
2023-04-17 14:01:41 +0800

    add main & routertest

---
+++ router/router_test.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetupRouter(t *testing.T) {
+	router := SetupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "pong", w.Body.String())
+}
