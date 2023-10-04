104e3b293830bc51c0849bce6d2d6b5169ac47bc
aimisaka <aimixsaka@gmail.com>
2023-10-04 14:06:49 +0800

    feat(oauth): add oauth lark client support

---
+++ util/net.go
@@ -0,0 +1,38 @@
+package util
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/NJUPT-SAST/sast-link-backend/log"
+)
+
+func PostWithHeader(url string, header map[string]string, body any) (*http.Response, error) {
+	jsonData, err := json.Marshal(body);
+	if err != nil {
+		log.Log.Errorln("json.Marshal ::: ", err)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Log.Errorln("http.NewRequest ::: ", err)
+		return nil, err
+	}
+	for k, v := range header {
+		req.Header.Set(k, v)
+	}
+
+	// DEBUG
+	log.LogReq(req)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Log.Errorln("http.DefaultClient.Do ::: ", err)
+		return nil, err
+	}
+
+	// DEBUG
+	log.LogRes(res)
+	return res, nil
+}
