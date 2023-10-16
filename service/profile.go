0cf05acf07af0bc498b20946f5d5b2aa22133423
windpo <windpo98@gmail.com>
2023-10-16 20:57:22 +0800

    add: avatar review deal

--- service/profile.go
+++ service/profile.go
@@ -1,16 +1,79 @@
 package service
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/NJUPT-SAST/sast-link-backend/model"
 	"github.com/NJUPT-SAST/sast-link-backend/model/result"
 	"github.com/NJUPT-SAST/sast-link-backend/util"
 	"github.com/gin-gonic/gin"
 	"mime/multipart"
+	"net/http"
 	"regexp"
 	"strconv"
 )
 
 var cos = util.T_cos
+
+const reviewFailMsg = `{
+        "msg_type": "post",
+        "content": {
+                "post": {
+                        "zh_cn": {
+                                "title": "腾讯云COS头像审核失败",
+                                "content": [
+                                        [{
+														"tag": "a",
+														"text": "图片",
+														"href": "%s"
+												},		
+												{
+                                                        "tag": "text",
+                                                        "text": "审核失败,后端开发人员:"
+                                                },
+                                                {
+                                                        "tag": "a",
+                                                        "text": "请查看",
+                                                        "href": "https://console.cloud.tencent.com/cos/bucket"
+                                                },
+												{		
+														"tag": "text",
+														"text": "。错误信息：%s" 
+												}
+                                        ]
+                                ]
+                        }
+                }
+        }
+}`
+const picSensitiveMsg = `{
+        "msg_type": "post",
+        "content": {
+                "post": {
+                        "zh_cn": {
+                                "title": "头像审核通知",
+                                "content": [
+                                        [
+												{
+														"tag": "a",
+														"text": "疑似敏感文件",
+														"href": "%s"
+												},
+												{
+                                                        "tag": "text",
+                                                        "text": "，建议人工复审/更改审核库: "
+                                                },
+                                                {
+                                                        "tag": "a",
+                                                        "text": "请查看",
+                                                        "href": "https://console.cloud.tencent.com/cos/bucket"
+                                                }
+                                        ]
+                                ]
+                        }
+                }
+        }
+}`
 
 func ChangeProfile(profile *model.Profile, uid string) error {
 	// check org_id
@@ -28,7 +91,7 @@ func ChangeProfile(profile *model.Profile, uid string) error {
 	// verify if profile exist by uid(student ID)
 	resProfile, err := model.SelectProfileByUid(uid)
 	if err != nil {
-		serviceLogger.Errorln("CheckProfileByUid Err", err)
+		serviceLogger.Errorln("CheckProfileByUid Err,ErrMsg:", err)
 		return err
 	}
 	if resProfile == nil {
@@ -38,7 +101,7 @@ func ChangeProfile(profile *model.Profile, uid string) error {
 
 	// update profile
 	if err := model.UpdateProfile(resProfile, profile); err != nil {
-		serviceLogger.Errorln("UpdateProfile Err", err)
+		serviceLogger.Errorln("UpdateProfile Err,ErrMsg:", err)
 		return err
 	}
 	return nil
@@ -47,7 +110,7 @@ func GetProfileInfo(uid string) (*model.Profile, error) {
 	// verify if profile exist by uid(student ID)
 	resProfile, err := model.SelectProfileByUid(uid)
 	if err != nil {
-		serviceLogger.Errorln("CheckProfileByUid Err", err)
+		serviceLogger.Errorln("CheckProfileByUid Err,ErrMsg:", err)
 		return nil, err
 	}
 	if resProfile == nil {
@@ -78,14 +141,14 @@ func GetProfileInfo(uid string) (*model.Profile, error) {
 func GetProfileOrg(OrgId int) (string, string, error) {
 	// check org_id
 	if OrgId > 26 {
-		serviceLogger.Errorln("org_id input Err")
+		serviceLogger.Errorln("org_id input Err,ErrMsg:")
 		return "", "", result.OrgIdError
 	} else if OrgId == 0 {
 		return "", "", nil
 	} else {
 		//get dep and org
 		if dep, org, err := model.GetDepAndOrgByOrgId(OrgId); err != nil {
-			serviceLogger.Errorln("GetDepAndOrgByOrgId Err", err)
+			serviceLogger.Errorln("GetDepAndOrgByOrgId Err,ErrMsg:", err)
 			return "", "", err
 		} else {
 			return dep, org, nil
@@ -93,43 +156,43 @@ func GetProfileOrg(OrgId int) (string, string, error) {
 	}
 }
 
-func UploadAvatar(avatar *multipart.FileHeader, uid string, ctx *gin.Context) error {
+func UploadAvatar(avatar *multipart.FileHeader, uid string, ctx *gin.Context) (string, error) {
 	//construct fileName
 	userInfo, userInfoErr := model.UserInfo(uid)
 	if userInfoErr != nil {
-		serviceLogger.Errorln("user not exist", userInfoErr)
-		return userInfoErr
+		serviceLogger.Errorln("user not exist,ErrMsg:", userInfoErr)
+		return "", userInfoErr
 	}
 	fileName := strconv.Itoa(int(userInfo.ID))
 
 	//get file stream
 	fd, fileIOErr := avatar.Open()
 	if fileIOErr != nil {
-		serviceLogger.Errorln("get file stream err", fileIOErr)
-		return fileIOErr
+		serviceLogger.Errorln("get file stream err,ErrMsg:", fileIOErr)
+		return "", fileIOErr
 	}
 	defer fd.Close()
 
 	//upload to cos
 	uploadKey := "/avatar/" + fileName + ".jpg"
 	if _, cosUpErr := cos.Object.Put(ctx, uploadKey, fd, nil); cosUpErr != nil {
-		serviceLogger.Errorln("upload avatar to cos fail", cosUpErr)
-		return cosUpErr
+		serviceLogger.Errorln("upload avatar to cos fail,ErrMsg:", cosUpErr)
+		return "", cosUpErr
 	}
 
 	//write to database, file url refer:tencent cos bucket file
 	if dBUpErr := model.UpdateAvatar("https://sast-link-1309205610.cos.ap-shanghai.myqcloud.com"+uploadKey, userInfo.ID); dBUpErr != nil {
 		//del cos file
 		if _, cosDelErr := cos.Object.Delete(ctx, uploadKey); cosDelErr != nil {
-			serviceLogger.Errorln("upload avatar to cos fail", cosDelErr)
-			return cosDelErr
+			serviceLogger.Errorln("upload avatar to cos fail,ErrMsg:", cosDelErr)
+			return "", cosDelErr
 		}
 
-		serviceLogger.Errorln("write file url to database Err", dBUpErr)
-		return dBUpErr
+		serviceLogger.Errorln("write file url to database Err,ErrMsg:", dBUpErr)
+		return "", dBUpErr
 	}
 
-	return nil
+	return "https://sast-link-1309205610.cos.ap-shanghai.myqcloud.com" + uploadKey, nil
 }
 
 func checkHideLegal(hide []string) error {
@@ -145,12 +208,70 @@ func checkHideLegal(hide []string) error {
 		matched, matchErr := regexp.MatchString(hideFiledPattern[0]+"|"+hideFiledPattern[1]+"|"+hideFiledPattern[2], hide[i])
 
 		if matchErr != nil {
-			serviceLogger.Errorln("match hide field Err", matchErr)
+			serviceLogger.Errorln("match hide field Err,ErrMsg:", matchErr)
 			return matchErr
 		} else if matched == false || i > len(hideFiledPattern) {
 			serviceLogger.Infof("hide field illegal")
 			return result.CheckHideIllegal
 		}
+	}
+	return nil
+}
+
+func SentMsgToBot(checkRes *model.CheckRes) error {
+	var message []byte
+	if checkRes.Code != 0 {
+		message = []byte(fmt.Sprintf(reviewFailMsg, checkRes.Data.Url, checkRes.Message))
+	} else if checkRes.Data.Result == 2 {
+		message = []byte(fmt.Sprintf(picSensitiveMsg, checkRes.Data.Url))
+	} else {
+		return nil
+	}
+
+	// set request
+	url := "https://open.feishu.cn/open-apis/bot/v2/hook/a569c93d-ef19-49ef-ba30-0b2ca73e4aa5"
+	req, reqErr := http.NewRequest("POST", url, bytes.NewBuffer(message))
+	req.Header.Set("Content-Type", "application/json")
+
+	// do request
+	client := &http.Client{}
+	resp, reqErr := client.Do(req)
+	if reqErr != nil {
+		serviceLogger.Errorln("Sent message to group bot fail,ErrMsg:", reqErr)
+		return reqErr
+	}
+	defer resp.Body.Close()
+	return nil
+}
+
+func DealWithFrozenImage(ctx *gin.Context, checkRes *model.CheckRes) error {
+	compileRegex := regexp.MustCompile("[0-9]+")
+	matchArr := compileRegex.FindAllString(checkRes.Data.Url, -1)
+	if matchArr == nil {
+		return result.PicURLErr
+	}
+	userId := matchArr[1]
+	source := "avatar/" + userId + ".jpg"
+	//mv image
+	sourceURL := fmt.Sprintf("sast-link-1309205610.cos.ap-shanghai.myqcloud.com/%s", source)
+	dest := "ban/" + userId + ".jpg"
+	_, _, err := cos.Object.Copy(ctx, dest, sourceURL, nil)
+	if err == nil {
+		_, err := cos.Object.Delete(ctx, source, nil)
+		if err != nil {
+			serviceLogger.Errorln("del file fail,ErrMsg:", err)
+			return err
+		}
+	} else {
+		serviceLogger.Errorln("cp file fail,ErrMsg:", err)
+	}
+
+	//replace image to Err image
+	avatarURL := "https://sast-link-1309205610.cos.ap-shanghai.myqcloud.com/ErrorImage.jpg"
+	parseUint, _ := strconv.Atoi(userId)
+	if upErr := model.UpdateAvatar(avatarURL, uint(parseUint)); upErr != nil {
+		serviceLogger.Errorln("updateAvatar fail,ErrMsg:", upErr)
+		return upErr
 	}
 	return nil
 }
