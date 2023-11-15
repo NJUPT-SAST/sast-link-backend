0e5b1c9ba5a26992f2eca79524718b925e9940a9
xun <xun794@gmail.com>
2023-11-15 20:10:23 +0800

    Add: request Limiter.
    Add request limiter for send email api and correct code style.

--- router/router_test.go
+++ router/router_test.go
@@ -3,6 +3,7 @@ package router
 import (
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,4 +16,22 @@ func TestInitRouter(t *testing.T) {
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, "pong", w.Body.String())
+}
+
+// Test limiter
+func TestLimiter(t *testing.T) {
+	router := InitRouter()
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("GET", "/api/v1/user/info", nil)
+			router.ServeHTTP(w, req)
+			//t.Log("result:" + w.Body.String())
+			assert.Equal(t, http.StatusOK, w.Code)
+		}()
+	}
+	wg.Wait()
 }
